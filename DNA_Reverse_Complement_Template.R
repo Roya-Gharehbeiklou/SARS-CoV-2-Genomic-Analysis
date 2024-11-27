11a5c187c6440117d5a650abadd09efbe52c75ed
Roya Gharehbeiklou <royagharehbeiklou@Royas-MacBook-Air.local>
2024-11-27 22:59:50 +0100

    the reverse file template added

---
+++ DNA_Reverse_Complement_Template.R
@@ -0,0 +1,51 @@
+# Adenine (A), Thymine (T), Cytosine (C), Guanine (G)
+# 
+# DNA complementary base pairing rule
+# A -> T
+# C -> G
+# G -> C
+# T -> A
+
+temp_DNA <- character()           # Template DNA Sequence
+reve_Sequence <- character()      # Reverse DNA Sequence
+comp_DNA <- character()           # Complementary DNA Sequence
+reve_comp_DNA <- character()      # Reverse-Complement DNA Sequence
+curr_base <- character()          # Holds one base from template DNA Sequence
+
+temp_DNA <- "ATGCCGCTAAACTGACATXXTCAGATC"
+
+#===== a- Reverse DNA ===========================================================
+reve_Sequence <- ''               # Make sure the reve_Sequence is empty
+# You need for loob here to reverse the DNA sequenc
+
+
+
+print(temp_DNA)
+print(reve_Sequence)
+#================================================================================
+
+
+#===== b- Complementary DNA Sequence ============================================
+comp_DNA <- ''                    # Make sure the comp_DNA is empty
+# You need 'for' loob to read the template DNA sequenc and 'if'
+# statments to add the complement base to the 'comp_DNA'
+
+
+
+
+
+print(temp_DNA)
+print(comp_DNA)
+# ===============================================================================
+
+
+# c- Reverse-Complement DNA Sequence ============================================
+reve_comp_DNA <- ''               # Make sure reve_comp_DNA is empty
+# You need 'for' loob to read the template DNA sequenc and 'if'
+# statments to add the complement base to the 'comp_DNA' but in 
+# reverse order
+
+
+print(temp_DNA)
+print(reve_comp_DNA)
+# ===============================================================================
